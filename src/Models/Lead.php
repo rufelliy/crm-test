52b06020c6ea373cc9c919770ddf289a186a1276
Andrii Reshetytskyi <reshetytskyi.a@digitalite.org>
2026-02-11 20:42:41 +0200

    remove timestams fields fron leads and managers

--- src/Models/Lead.php
+++ src/Models/Lead.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lead extends Model
 {
+    public $timestamps = false;
     protected $fillable = [
         'name',
         'phone',
--- src/Models/Manager.php
+++ src/Models/Manager.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Manager extends Model
 {
+    public $timestamps = false;
     protected $fillable = ['name'];
 
     public function leads()
